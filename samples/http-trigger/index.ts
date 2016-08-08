24ebfb8036ecdcc8374ccf4aa5090b6d6297b17d
Chris Anderson <chrande@outlook.com>
2016-08-08 03:09:08 -0700

    adjust npm and typings install to work

--- samples/http-trigger/index.ts
+++ samples/http-trigger/index.ts
@@ -1,6 +1,4 @@
-/// <reference path="../typings/index.d.ts" />
-
-import {HttpContext, IFunctionRequest, HttpStatusCodes} from 'azure-functions'
+import {HttpContext, IFunctionRequest, HttpStatusCodes} from 'azure-functions-typescript'
 
 export function index(context: HttpContext, req: IFunctionRequest) {
     if(req.method == "GET")
