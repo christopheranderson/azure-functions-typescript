532b12e6c0995bc41944551e8e641af5ef2ea6c0
Chris Feijoo <christophe@feijoo.fr>
2017-03-19 16:26:39 +0100

    Use @types and clean

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import {Context} from './context';
-import {IFunctionRequest, IFunctionResponse, HttpContext, HttpStatusCodes, HttpMethod} from './http';
-
-
-export {Context, IFunctionRequest, IFunctionResponse, HttpContext, HttpStatusCodes, HttpMethod};
+export * from './context'
+export * from './http'
